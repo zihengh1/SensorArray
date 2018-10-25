9ea6831d03c9527176105f84b0b805929663448e
zihengh1 <105703031@nccu.edu.tw>
2018-10-25 14:19:43 +0800

    update

--- reboot.sh
+++ reboot.sh
@@ -4,11 +4,9 @@ sleep 1
 [ -f /home/pi/SensorArray/sensor.py ] && {
     /usr/bin/git -C /home/pi/SensorArray fetch origin
     /usr/bin/git -C /home/pi/SensorArray reset --hard origin/master
-    echo $(date +'%Y-%m-%d %H:%M:%S')
     nohup python -u /home/pi/SensorArray/test_program.py &
 } || {
     /usr/bin/git clone https://github.com/zihengh1/SensorArray/ /home/pi/SensorArray
-    echo $(date +'%Y-%m-%d %H:%M:%S')
     nohup python -u /home/pi/SensorArray/test_program.py &
 }
 
