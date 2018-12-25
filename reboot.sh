090076dafcfd551f168ceb9fa7f33fbe1476d958
Tzu-Heng, Huang <35376020+zihengh1@users.noreply.github.com>
2018-12-25 09:40:12 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sleep 1
+sleep 5
 [ -f /home/pi/SensorArray/sensor.py ] && {
-    /usr/bin/git -C /home/pi/SensorArray fetch origin
-    /usr/bin/git -C /home/pi/SensorArray reset --hard origin/master
-    sudo nohup python -u /home/pi/SensorArray/sensor.py &
+    /usr/bin/sudo git -C /home/pi/SensorArray fetch origin
+    /usr/bin/sudo git -C /home/pi/SensorArray reset --hard origin/master
+    /usr/bin/sudo nohup python -u /home/pi/SensorArray/sensor.py &
 } || {
-    /usr/bin/git clone https://github.com/zihengh1/SensorArray/ /home/pi/SensorArray
-    sudo nohup python -u /home/pi/SensorArray/sensor.py &
+    /usr/bin/sudo git clone https://github.com/zihengh1/SensorArray/ /home/pi/SensorArray
+    /usr/bin/sudo nohup python -u /home/pi/SensorArray/sensor.py &
 }
 
