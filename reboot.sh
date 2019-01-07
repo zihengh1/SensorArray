5b26d3b52dc799b536788fe7c02146406eab95c7
zihengh1 <105703031@nccu.edu.tw>
2019-01-07 02:58:57 +0000

    modify reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -2,11 +2,11 @@
 
 sleep 1
 [ -f /home/pi/SensorArray/sensor.py ] && {
-    /usr/bin/git -C /home/pi/SensorArray fetch origin
-    /usr/bin/git -C /home/pi/SensorArray reset --hard origin/master
-    nohup python -u /home/pi/SensorArray/sensor.py &
+    /usr/bin/sudo git -C /home/pi/SensorArray fetch --all
+    /usr/bin/sudo git -C /home/pi/SensorArray reset --hard origin/master
+    /usr/bin/sudo nohup python -u /home/pi/SensorArray/sensor.py &
 } || {
     /usr/bin/git clone https://github.com/zihengh1/SensorArray/ /home/pi/SensorArray
-    nohup python -u /home/pi/SensorArray/sensor.py &
+    /usr/bin/sudo nohup python -u /home/pi/SensorArray/sensor.py &
 }
 
